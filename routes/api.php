d2377c006aa89ee3cf90fe6979e9b54faf4a9436
Avo <75686081+AvoDieKatz@users.noreply.github.com>
2023-03-14 17:04:58 +0700

    Put back authentication

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,7 @@ Route::apiResource('categories', CategoryController::class);
 Route::apiResource('departments', DepartmentController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('events', EventController::class);
+Route::apiResource('ideas', IdeaController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     // Content temporary moved out
